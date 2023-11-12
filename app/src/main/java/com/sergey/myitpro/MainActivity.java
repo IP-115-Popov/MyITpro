422109654a4f1cbf8280783acf37a59c0b642b3f
Попов Сергей Андреевич <ip115s19@csc.sibsutis.ru>
2023-11-12 14:26:05 +0700

    Added the ability to add

--- app/src/main/java/com/sergey/myitpro/MainActivity.java
+++ app/src/main/java/com/sergey/myitpro/MainActivity.java
@@ -3,18 +3,34 @@ package com.sergey.myitpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textResultView;
+    EditText number_field_1, number_field_2;
+    Button add_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textResultView = findViewById(R.id.textResultView);
+        number_field_1 = findViewById(R.id.number_field_1);
+        number_field_2 = findViewById(R.id.number_field_2);
+        add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float num1 = Float.parseFloat(number_field_1.getText().toString());
+                float num2 = Float.parseFloat(number_field_2.getText().toString());
+                float res = num1+num2;
+                textResultView.setText(String.valueOf(res));
+            }
+        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        
-    }
 }
